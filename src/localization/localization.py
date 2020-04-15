363cc06818eacf28eaf99750a84dfade06917bda
Sravan Balaji <balajsra@umich.edu>
2020-04-15 17:37:28 -0400

    gitignore & Read Velodyne Data
    - Add gitignore so VS Code and dataset files are not committed
    - Add python functions provided by NCLT dataset for reading velodyne
      data

---
+++ src/localization/localization.py
@@ -0,0 +1,16 @@
+# localization.py
+
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import os
+from PoseList import PoseList
+from scipy.spatial import distance
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
